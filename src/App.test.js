1635db201e717d82f1fd87ec91ce4899a8577585
doobix <doobix@gmail.com>
2018-03-27 19:30:05 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import setupBrowserMocks from './utils/browserMocks';
+
+setupBrowserMocks();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
